8df79768c2a290ae1152aa55a73d9c23d5231799
Alexander Vasquez <josuealcantara251@gmail.com>
2025-02-04 23:15:41 -0500

    Arreglando historial

--- project1/controllers/historial_controller.py
+++ project1/controllers/historial_controller.py
@@ -2,8 +2,13 @@ from PyQt6.QtWidgets import QWidget  # type:ignore
 from views.historial.Ui_Form_Historial import Ui_Form_Historial
 
 class HistorialController:
-    def __init__(self, historial_model):
-        self.historial_model = historial_model
+    def __init__(self):
+        self.ventana_historial = QWidget()
+        self.ui = Ui_Form_Historial()
+        self.ui.setupUi(self.ventana_historial)
+        self.ventana_principal_controller = None
+
+        self.ui.btn_regresar.clicked.connect(self.regresar_ventana_prinicipal)
 
     def obtener_datos_historial(self):
         # Obtener datos del modelo
@@ -12,3 +17,11 @@ class HistorialController:
     def actualizar_estado_reserva(self, id_reserva, nuevo_estado):
         # Llamar al modelo para actualizar la base de datos
         self.historial_model.actualizar_estado_reserva(id_reserva, nuevo_estado)
+
+    def regresar_ventana_prinicipal(self):
+        self.ventana_historial.hide()
+        if self.ventana_principal_controller:
+            self.ventana_principal_controller.mostrar_ventana()
+
+    def mostrar_ventana(self):
+        self.ventana_historial.show()
